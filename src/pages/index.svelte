0926992cbde893f10b4f5165f081ce63888be80b
Fidel Choque <devcrash03@gmail.com>
2023-01-28 19:56:48 +0100

    Add complement CardTopTen a index

--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -1,13 +1,14 @@
 <script lang="ts">
+  import CardTopTen from "../components/elements/CardTopTen.svelte";
   let count: number = 0;
 </script>
 
 <main
-  class="bg-heroFit bg-cover w-full h-full flex items-center justify-center flex-col gap-3 text-4xl"
->
+  class="bg-heroFit bg-cover w-full h-full flex items-center justify-center flex-col gap-3 text-4xl">
   <p class="text-white">SE TU MEJOR</p>
   <section class="container mx-auto mt-5 text-center">
     <div class="grid grid-cols-1 sm:grid-cols-3 gap-3">
+        <CardTopTen/>
         <div class="bg-red-400 row-span-5">
           <h2 class="text-2xl">Top ten Gym</h2>
           <div class="bg-slate-200 rounded-xl m-3">
@@ -26,90 +27,28 @@
           </div>
           
         </div>
+
         <div class="bg-red-400">
           <spam class="text-2xl">The best Gym reviews</spam>
           <div class="flex justify-center m-3">
             <div class="block p-6 rounded-lg shadow-lg bg-slate-200 max-w-sm">
               <h5 class="text-gray-900 text-xl leading-tight font-medium mb-2">Gimnasio Fitup</h5>
-              <p class="text-gray-700 text-base mb-4">Some quick example text to build on the card title and make up the bulk of the card's
-      content.</p>
+              <p class="text-gray-700 text-base mb-4">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
             </div>
           </div>
-          </div>
+        </div>
+
         <div class="bg-red-400">
           <spam class="text-2xl">Latest Gym Comments</spam>
           <div class="flex justify-center m-3">
             <div class="block p-6 rounded-lg shadow-lg bg-slate-200 max-w-sm">
               <h5 class="text-gray-900 text-xl leading-tight font-medium mb-2">Gimnasio Fitup</h5>
-              <p class="text-gray-700 text-base mb-4">Some quick example text to build on the card title and make up the bulk of the card's
-      content.</p>
+              <p class="text-gray-700 text-base mb-4">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
             </div>
           </div>
         </div>
     </div>
-    <!-- <div class="p-5 screen bg-gray-100">
-      <h1 class="text-xl mb-2">Top Ten Gym</h1>
-  
-      <div class="overflow-auto rounded-lg shadow hidden md:block">
-        <table class="w-full">
-          <thead class="bg-gray-50 border-b-2 border-gray-200">
-          <tr>
-            <th class="w-20 p-3 text-sm font-semibold tracking-wide text-left">No.</th>
-            <th class="p-3 text-sm font-semibold tracking-wide text-left">Name Gym</th>
-            <th class="w-24 p-3 text-sm font-semibold tracking-wide text-left">Direction</th>
-           
-          </tr>
-          </thead>
-          <tbody class="divide-y divide-gray-100">
-          <tr class="bg-white">
-            <td class="p-3 text-sm text-gray-700 whitespace-nowrap">
-              <a href="#" class="font-bold text-blue-500 hover:underline">1</a>
-            </td>
-            <td class="p-3 text-sm text-gray-700 whitespace-nowrap">
-              Name Gym 1
-            </td>
-            <td class="p-3 text-sm text-gray-700 whitespace-nowrap">
-            <span
-              class="p-1.5 text-xs font-medium uppercase tracking-wider text-green-800 bg-green-200 rounded-lg bg-opacity-50">Direccion Gym 1</span>
-            </td>
-            
-          </tr>
-          <tr class="bg-gray-50">
-            <td class="p-3 text-sm text-gray-700 whitespace-nowrap">
-              <a href="#" class="font-bold text-blue-500 hover:underline">2</a>
-            </td>
-            <td class="p-3 text-sm text-gray-700 whitespace-nowrap">Name Gym 2</td>
-            <td class="p-3 text-sm text-gray-700 whitespace-nowrap">
-            <span
-              class="p-1.5 text-xs font-medium uppercase tracking-wider text-yellow-800 bg-yellow-200 rounded-lg bg-opacity-50">Direccion Gym 2</span>
-            </td>
-            
-          </tr>
-          <tr class="bg-white">
-            <td class="p-3 text-sm text-gray-700 whitespace-nowrap">
-              <a href="#" class="font-bold text-blue-500 hover:underline">3</a>
-            </td>
-            <td class="p-3 text-sm text-gray-700 whitespace-nowrap">Name Gym 3</td>
-            <td class="p-3 text-sm text-gray-700 whitespace-nowrap">
-            <span
-              class="p-1.5 text-xs font-medium uppercase tracking-wider text-gray-800 bg-gray-200 rounded-lg bg-opacity-50">Direccion Gym 3</span>
-            </td>
-            
-          </tr>
-          </tbody>
-        </table>
-      </div>
-
-      <div class="bk-slate-400 p-7 m-1 rounded border-black ">
-        <p>The best Gym reviews</p>
-      </div>
-
-      <div class="p-7 m-1">
-        <p>Latest Gym Comments</p>
-      </div>
-  
-      
-    </div> -->
+    
   </section>
 
 
